2ad7161f7cf7732c9c9ff323605e972d3da8663e
hanahorvat <hana.lia.horvat@gmail.com>
2026-05-27 10:18:41 +0200

    add jsdom dependency for vitest environment

--- web-template/vite.config.ts
+++ web-template/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
@@ -33,4 +33,17 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    server: {
+      deps: {
+        inline: [/@tailwindcss\/vite/],
+      }
+    }
+  }
 })
