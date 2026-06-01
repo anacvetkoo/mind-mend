2169ab20eef50484aeb91b5bea007a07568683e0
hanahorvat <hana.lia.horvat@gmail.com>
2026-06-01 14:59:30 +0200

    spremenjen vite.config za deploy

--- web-template/vite.config.ts
+++ web-template/vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -17,6 +16,8 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/',
+
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
@@ -27,7 +28,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src')
     },
   },
 
@@ -47,3 +48,53 @@ export default defineConfig({
     }
   }
 })
+
+// import { defineConfig } from 'vitest/config'
+// import path from 'path'
+// import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react'
+
+
+// function figmaAssetResolver() {
+//   return {
+//     name: 'figma-asset-resolver',
+//     resolveId(id) {
+//       if (id.startsWith('figma:asset/')) {
+//         const filename = id.replace('figma:asset/', '')
+//         return path.resolve(__dirname, 'src/assets', filename)
+//       }
+//     },
+//   }
+// }
+
+// export default defineConfig({
+//   plugins: [
+//     figmaAssetResolver(),
+//     // The React and Tailwind plugins are both required for Make, even if
+//     // Tailwind is not being actively used – do not remove them
+//     react(),
+//     tailwindcss(),
+//   ],
+//   resolve: {
+//     alias: {
+//       // Alias @ to the src directory
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+
+//   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+//   assetsInclude: ['**/*.svg', '**/*.csv'],
+
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//     server: {
+//       deps: {
+//         inline: [/@tailwindcss\/vite/],
+//       }
+//     }
+//   }
+// })
